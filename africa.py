12c9fca6da79b75d26d0b608f1694ac0c0ccb687
Brian <brian@brian@gmail.com>
2025-03-03 20:29:21 +0300

    Updated the file with new modifications

--- africa.py
+++ africa.py
@@ -2,7 +2,7 @@ def main():
     """
     Main function to execute the program.
     """
-    print("Welcome to the Tanzania program!")
+    print("Welcome to the Africa program!")
 
 if __name__ == "__main__":
     main()
